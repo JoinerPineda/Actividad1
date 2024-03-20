dd393950c9c7a60625367942a0843210aaa3d62b
Joiner Pineda <joinnerpineda@gmail.com>
2024-03-20 17:34:47 -0500

    session 5

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        GradeBook gradeBook1 = new GradeBook("CS101 Introduction to Java Programming");
+        GradeBook gradeBook2 = new GradeBook("CS102 Data Structures in Java");
+
+        gradeBook1.displayMessage();
+        gradeBook2.displayMessage();
+    }
+}
